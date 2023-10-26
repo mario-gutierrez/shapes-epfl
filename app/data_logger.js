05f1032ca8c1ea89900593c8d6b64566401b4cb1
Mario Gutierrez <mgutierrez1@logitech.com>
2023-10-26 12:06:49 +0200

    Log data as a json array

--- app/data_logger.js
+++ app/data_logger.js
@@ -1,20 +1,23 @@
 class DataLogger {
     constructor(logger, filePrefix) {
+        this.separator = "";
         this.logger = logger;
         this.logger.LogSettings.fileName = filePrefix;
     }
 
     startLog() {
         this.logger.openLogFile();
-        this.logger.log("csvheader");
-
+        this.logger.log("[");
+        this.separator = "";
     }
 
     logData(data) {
-        this.logger.log(data);
+        this.logger.log(this.separator + data);
+        this.separator = ",";
     }
 
     stopLog() {
+        this.logger.log("]");
         this.logger.closeLogFile();
     }
 
@@ -22,5 +25,6 @@ class DataLogger {
         return this.logger.logFileIsOpen();
     }
 }
+
 
 module.exports = DataLogger;
