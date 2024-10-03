c4d34ffc766e374815ed603cf1a532a2f3db9038
Mario Gutierrez <mgutierrez1@logitech.com>
2024-10-03 21:33:16 +0200

    Add non-recursive Fill algorithm

--- app/public_html/js/drawing_engine.js
+++ app/public_html/js/drawing_engine.js
@@ -6,12 +6,15 @@ class DrawingEngine {
         this.websocket = websocket;
         this.canvas = canvas;
         this.previousPoint = undefined;
-        this.lineColorRGB = [16, 16, 16];
+        this.lineColorRGB = [10, 10, 11, 255];
         this.lineColor = this.RGBtoHexString(this.lineColorRGB);
         this.currentImageData = undefined;
     }
     RGBtoHexString(color) {
-        return `#${color[0].toString(16)}${color[1].toString(16)}${color[2].toString(16)}`;
+        const getHexString = (n) => {
+            return (n < 16 ? '0' : '') + n.toString(16);
+        }
+        return `#${getHexString(color[0])}${getHexString(color[1])}${getHexString(color[2])}${getHexString(color[3])}`;
     }
     DrawSquare(center, size, color) {
         let s = size / 2;
@@ -52,7 +55,7 @@ class DrawingEngine {
         const screenCoords = [point.offsetX, point.offsetY];
 
         if (this.previousPoint) {
-            this.DrawVector(screenCoords, [this.previousPoint.offsetX, this.previousPoint.offsetY], 0, 3, color);
+            this.DrawVector(screenCoords, [this.previousPoint.offsetX, this.previousPoint.offsetY], 0, 3, this.lineColor);
         }
         this.DrawSquare(screenCoords, point.pressure * 3, color);
         this.previousPoint = point;
@@ -61,57 +64,84 @@ class DrawingEngine {
         }
         logArea.innerHTML = JSON.stringify(point);
     }
-    GetColorIndicesForCoord = (x, y) => {
+    GetColorIndicesForCoord = (p) => {
+        const x = p[0];
+        const y = p[1];
         if (x > 0 && x < this.canvas.width && y > 0 && y < this.canvas.height) {
             const i = y * (this.canvas.width * 4) + x * 4;
             return [i, i + 1, i + 2, i + 3];
         }
-        console.error(`Coordinates out of image bounds: [${x},${y}]`);
-        return undefined;
+        //console.error(`Coordinates out of image bounds: [${x},${y}]`);
+        return [];
     }
-    ColorPixelIs(x, y, color) {
-        const indices = this.GetColorIndicesForCoord(x, y);
-        if (indices !== undefined) {
+    ColorPixelIs(p, color) {
+        const indices = this.GetColorIndicesForCoord(p);
+        if (indices.length == 4) {
             const pixelColor = [this.currentImageData[indices[0]], this.currentImageData[indices[1]], this.currentImageData[indices[2]]];
-            if (
-                pixelColor[0] === color[0] &&
-                pixelColor[1] === color[1] &&
-                pixelColor[2] === color[2]
-            ) {
-                return true;
+
+            const dist = Math.hypot(pixelColor[0] - color[0], pixelColor[1] - color[1], pixelColor[2] - color[2]);
+            if (dist < 15) {
+                return 1;
             }
+            return 0;
         }
-        //console.error('color pixel is undefined');
-        return false;
+        console.error('color pixel is undefined');
+        return 2;
     }
     Fill(point, color = [0, 128, 250]) {
         const imageData = this.context.getImageData(0, 0, this.canvas.width, this.canvas.height);
         this.currentImageData = imageData.data;
 
         const p = [point.offsetX, point.offsetY];
-        this.FillPixel([p[0], p[1]], color);
+
+        const pointsStack = [];
+        pointsStack.push(p);
+
+        while (pointsStack.length > 0) {
+            const p = pointsStack.pop();
+            if (this.FillPixel(p, color)) {
+
+                let newPoint = [p[0], p[1] - 1];
+                if (this.GetColorIndicesForCoord(newPoint).length == 4) {
+                    pointsStack.push(newPoint);
+                }
+
+                newPoint = [p[0], p[1] + 1];
+                if (this.GetColorIndicesForCoord(newPoint).length == 4) {
+                    pointsStack.push(newPoint);
+                }
+
+                newPoint = [p[0] - 1, p[1]];
+                if (this.GetColorIndicesForCoord(newPoint).length == 4) {
+                    pointsStack.push(newPoint);
+                }
+
+                newPoint = [p[0] + 1, p[1]];
+                if (this.GetColorIndicesForCoord(newPoint).length == 4) {
+                    pointsStack.push(newPoint);
+                }
+            }
+        }
+
         this.context.putImageData(imageData, 0, 0);
     }
-    TryToFillPixel(newPoint, color) {
-        if (this.ColorPixelIs(newPoint[0], newPoint[1], this.lineColorRGB) === false &&
-            this.ColorPixelIs(newPoint[0], newPoint[1], color) === false) {
-            this.FillPixel([newPoint[0], newPoint[1]], color);
+    PixelShouldBeFilled(newPoint, color) {
+        if (this.ColorPixelIs(newPoint, this.lineColorRGB) === 0 &&
+            this.ColorPixelIs(newPoint, color) === 0) {
+            return true;
         }
+        return false;
     }
     FillPixel(point, color) {
-        if (this.ColorPixelIs(point[0], point[1], this.lineColorRGB) === false &&
-            this.ColorPixelIs(point[0], point[1], color) === false) {
-            const indices = this.GetColorIndicesForCoord(point[0], point[1]);
+        if (this.PixelShouldBeFilled(point, color)) {
+            const indices = this.GetColorIndicesForCoord(point);
             //console.log(`[${point[0]},${point[1]}]`);
             this.currentImageData[indices[0]] = color[0];
             this.currentImageData[indices[1]] = color[1];
             this.currentImageData[indices[2]] = color[2];
             this.currentImageData[indices[3]] = 255;
-
-            this.TryToFillPixel([point[0], point[1] - 1], color);
-            this.TryToFillPixel([point[0], point[1] + 1], color);
-            this.TryToFillPixel([point[0] - 1, point[1]], color);
-            this.TryToFillPixel([point[0] + 1, point[1]], color);
+            return true;
         }
+        return false;
     }
 }
