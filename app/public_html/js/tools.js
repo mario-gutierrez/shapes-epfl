655af73db5fecbec87f04d35fc891cba71f5659f
Mario Gutierrez <mgutierrez1@logitech.com>
2024-10-11 11:39:03 +0200

    Add tools.js with functions to handle url params and post requests

---
+++ app/public_html/js/tools.js
@@ -0,0 +1,32 @@
+function Tools_getParameterByName(name) {
+    const url = window.location.href;
+    name = name.replace(/[\[\]]/g, "\\$&");
+    const regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)");
+    const results = regex.exec(url);
+    if (!results) return null;
+    if (!results[2]) return '';
+    return decodeURIComponent(results[2].replace(/\+/g, " "));
+}
+
+function Tools_postData(url, jsonObject) {
+    return fetch(url, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(jsonObject),
+    })
+        .then(response => {
+            if (!response.ok) {
+                throw new Error(`HTTP error! Status: ${response.status}`);
+            }
+            return response.json(); // Parse the JSON response from the server
+        })
+        .then(data => {
+            //console.log("Response from POST request:", data);
+            return data; // Return the JSON object received from the server
+        })
+        .catch(error => {
+            console.error("Error:", error);
+        });
+}
